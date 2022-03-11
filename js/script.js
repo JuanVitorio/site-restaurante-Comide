d5a4880ab32f4c50c3ffb59e13c7f49a4e501d72
Whoan10 <juanvitorioclash@gmail.com>
2022-03-11 18:25:24 -0300

    arrumei as coisa

--- js/script.js
+++ js/script.js
@@ -6,3 +6,27 @@ hamburguer.addEventListener('click', function(){
   document.querySelector('.line2').classList.toggle('bg-color');
   document.querySelector('.line3').classList.toggle('bg-color');
 })
+
+function topo(){
+  window.scroll({
+    top:0, behavior: 'smooth'
+  })
+}
+
+function mid(){
+  window.scroll({
+    top:650, behavior:'smooth'
+  })
+}
+
+function low(){
+  window.scroll({
+    top:1450, behavior:'smooth'
+  })
+}
+
+function foot(){
+  window.scroll({
+    top:2050, behavior:'smooth'
+  })
+}
